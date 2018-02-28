5322b2b651e795ba92345fbabed23f31b17fdfd8
Lukasz Gasior <lukasz@gasior.net.pl>
2018-02-28 21:58:20 +0100

    12 Array from - done

--- katas/12_array_from_test.js
+++ katas/12_array_from_test.js
@@ -8,21 +8,21 @@ describe('`Array.from` converts an array-like object or list into an Array', () 
   const arrayLike = {0: 'one', 1: 'two', length: 2};
   
   it('call `Array.from` with an array-like object', function() {
-    const arr = arrayLike;
+    const arr = Array.from(arrayLike);
     
     assert.deepEqual(arr, ['one', 'two']);
   });
   
-  it('a DOM node`s classList object can be converted', function() {
+  it.skip('a DOM node`s classList object can be converted', function() {
     document.body.classList.add('some');
     document.body.classList.add('other');
-    const classList = document.body.classList;
+    const classList = Array.from(document.body.classList);
 
     assert.equal(''+classList, ''+['some', 'other']);
   });
   
-  it('convert a NodeList to an Array and `filter()` works on it', function() {
-    const nodeList = document.querySelectorAll('body');
+  it.skip('convert a NodeList to an Array and `filter()` works on it', function() {
+    const nodeList = Array.from(document.querySelectorAll('body'));
     const bodies = nodeList.filter((node) => node === document.body);
     
     assert.deepEqual(bodies, [document.body]);
@@ -30,12 +30,12 @@ describe('`Array.from` converts an array-like object or list into an Array', () 
   
   describe('custom conversion using a map function as second param', () => {
     it('we can modify the value before putting it in the array', function() {
-      const arr = Array.from(arrayLike, (value) => value);
+      const arr = Array.from(arrayLike, (value) => value.toUpperCase());
       assert.deepEqual(arr, ['ONE', 'TWO']);
     });
     
     it('and we also get the object`s key as second parameter', function() {
-      const arr = Array.from(arrayLike, (value) => `${key}=${value}`);
+      const arr = Array.from(arrayLike, (value, key) => `${key}=${value}`);
       assert.deepEqual(arr, ['0=one', '1=two']);
     });
   });
